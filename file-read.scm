061003970c85c9bf119907c7700a5fda5a1a891f
Maury2001 <mmakira01@gmail.com>
2023-11-01 17:35:35 +0300

    ff

--- file-read.scm
+++ file-read.scm
@@ -4,20 +4,22 @@
 ;(read-line in)
 ;(close-input-port in)
 
-(define file "Desktop/files.py")
+
+
+
+(define file "files.py")
 
 
 (define (read-file file-name)
-  
   (let ((p (open-input-file file-name)))
-    (let loop((ls1 '()) (c (read-char p)))
+    (let loop ((ls1 '()) (c (read-char p)))
       (if (eof-object? c)
-	  (begin
-	    (close-input-port p)
-	    (list->string (reverse ls1)))
-	  (loop (cons c ls1) (read-char p))))))
+          (begin
+            (close-input-port p)
+            (list->string (reverse ls1)))
+          (loop (cons c ls1) (read-char p))))))
 
 (read-file file)
-(define content (read-file "Desktop/files.py"))
+(define content (read-file file))
 (string? content)
 (display content)
--- test.scm
+++ test.scm
@@ -1,4 +1,55 @@
 #lang scheme 
-(eqv? 3 3)
 
-(eol-style symbol)
+
+(define (shout display-fn txt)
+  (display-fn
+   (list->string
+    (map
+     char-upcase
+     (string->list txt)))))
+
+(shout display "your modda")
+
+
+(define (make-balance)
+  (define value 0)
+  (define (bal method)
+    (define (add-method x)
+      (set! value (+ value x)))
+    (define (get-method) value)
+      (if (equal? method "add")
+          add-method
+          get-method))
+    bal)
+
+
+(define a (make-balance ))
+(define b (make-balance ))
+(newline)
+
+((a "add") 3)
+((b "add") 2)
+((b "get"))
+((a "get"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
